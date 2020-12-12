d88041f690f17f9a77c969f3b720fe5741b865eb
Matthew Foulk <mewuzhere12@gmail.com>
2020-12-12 11:29:48 -0500

    Responds to cap

--- bot.py
+++ bot.py
@@ -6,11 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CAP_ID = 150368048627318786
+MY_ID = 743531706245578762
 
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print(f"{client.user} has connected to Discord!")
+async def on_message(message):
+    if message.author.id != CAP_ID:
+        return
+
+    capWarning = "***Some or all of the content shared in the above message may be misleading***"
+    await message.channel.send(capWarning)
 
 client.run(TOKEN)
